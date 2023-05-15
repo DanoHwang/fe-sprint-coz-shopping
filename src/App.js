f68aa1b44c68f1e2e3fac39b078cdbdcf9766d4a
DanoHwang <danohwang00@gmail.com>
2023-05-15 13:39:13 +0900

    Feat: Create Header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
 import Footer from './pages/Footer';
+import Header from './pages/Header';
 
 function App() {
 
   return (
       <div>
+        <Header />
         <Routes>
           <Route path='/' element={<MainPage />} />
         </Routes>
