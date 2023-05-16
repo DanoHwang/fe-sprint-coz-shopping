d50142cf0137c70df4222819dd2ff6ece714175a
DanoHwang <danohwang00@gmail.com>
2023-05-16 13:04:19 +0900

    Feat: Add  MainPage funtion to load bookmark list, Align MainPa ge

--- src/components/ItemCardList.js
+++ src/components/ItemCardList.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import ItemCard from '../components/ItemCard';
-import { productsData } from '../assets/dummy';
+import { styled } from 'styled-components';
 
-export default function ItemCardList ({ count, bookmarks, setBookmarks }) {
+import { productsData } from '../assets/dummy';
+import { itemCardTypes } from '../constants/itemCardTypes';
+
+import ItemCard from '../components/ItemCard';
+
+const { all, bookmark } = itemCardTypes;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+  margin: 12px 0;
+`;
+
+export default function ItemCardList ({ count, bookmarks, setBookmarks, searchType = all }) {
+  const searchData = (type) => {
+    if (type === all) return productsData;
+
+    if (type === bookmark) {
+      return bookmarks.map((id) => productsData.find((data) => data.id === id));
+    }
+
+    return productsData.filter((data) => data.type === searchType);
+  };
+
+  const filteredData = searchData(searchType);
 
   return (
-    <div>
-      {productsData.slice(0, count).map((data) => (
+    <Wrapper>
+      {filteredData.slice(0, count).map((data) => (
         <ItemCard
           data={data}
           key={data.id}
@@ -14,6 +40,6 @@ export default function ItemCardList ({ count, bookmarks, setBookmarks }) {
           setBookmarks={setBookmarks}
         />
       ))}
-    </div>
-  )
+    </Wrapper>
+  );
 }
