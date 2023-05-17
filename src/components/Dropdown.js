250a1f5b1e70277a6a3fe084d948a8f8d9dc5dd2
DanoHwang <danohwang00@gmail.com>
2023-05-17 11:22:07 +0900

    Refactor: Separate styled-component code from Dropdown and Header components

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,61 +1,22 @@
 import React from 'react';
-import { styled } from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Menu = styled.div`
-  width: 200px;
-  height: 150px;
-  border-radius: 12px;
-  box-shadow: 0px 0px 8px 0px #0000001A;
-  background-color: #FFFF;
-  position: absolute;
-  top: 87px;
-  right: 32px;
-`;
-
-const Triangle = styled.div`
-  width: 25px;
-  height: 25px;
-  background-color: #FFFF;
-  box-shadow: -3px 3px 8px -3px #0000001A;
-  transform: rotate(135deg);
-  position: absolute;
-  top: 75px;
-  right: 80px;
-  z-index: 1;
-`;
-
-const MenuItem = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 200px;
-  height: 50px;
-  border-bottom: 0.5px solid #0000001A;
-
-  &:last-child {
-    border-bottom: none;
-  }
-
-  &:hover:not(:first-child) {
-    cursor: pointer;
-  }
-`;
+import * as S from '../styles/DropdownStyle';
 
 export default function Dropdown ({ handleMenuOpen }) {
 
   return (
     <>
-      <Triangle />
-      <Menu>
-        <MenuItem>OOO님, 안녕하세요!</MenuItem>
-        <MenuItem>
+      <S.Triangle />
+      <S.Menu>
+        <S.MenuItem>OOO님, 안녕하세요!</S.MenuItem>
+        <S.MenuItem>
           <Link to='/products/list' onClick={handleMenuOpen}>상품리스트 페이지</Link>
-        </MenuItem>
-        <MenuItem>
+        </S.MenuItem>
+        <S.MenuItem>
           <Link to='/bookmark' onClick={handleMenuOpen}>북마크 페이지</Link>
-        </MenuItem>
-      </Menu>
+        </S.MenuItem>
+      </S.Menu>
     </>
   );
 }
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,44 +1,11 @@
 import React, { useState } from 'react';
-import { styled } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 import logoImg from '../assets/logo.svg';
 import menuImg from '../assets/hamburger-button.svg';
 import Dropdown from '../components/Dropdown';
 
-const Head = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
-  position: sticky;
-`;
-
-const LogoWrapper = styled.div`
-  margin: 25px 0;
-  cursor: pointer;
-`;
-
-const LogoImage = styled.img`
-  width: 55px;
-  height: 30px;
-  margin-left: 76px;
-`;
-
-const Title = styled.div`
-  display: inline-block;
-  margin-left: 12px;
-  font-weight: 700;
-  font-size: 32px;
-`;
-
-const HamburgerButton = styled.img`
-  width: 34px;
-  height: 24px;
-  margin-right: 76px;
-  position: relative;
-  cursor: pointer;
-`;
+import * as S from '../styles/HeaderStyle';
 
 export default function Header () {
   const [ isMenuOpened, setIsMenuOpened ] = useState(false);
@@ -53,13 +20,13 @@ export default function Header () {
   };
 
   return (
-    <Head>
-      <LogoWrapper onClick={handleLogoClick}>
-        <LogoImage src={logoImg} />
-        <Title>COZ Shopping</Title>
-      </LogoWrapper>
-      <HamburgerButton src={menuImg} onClick={handleMenuOpen} />
+    <S.Head>
+      <S.LogoWrapper onClick={handleLogoClick}>
+        <S.LogoImage src={logoImg} />
+        <S.Title>COZ Shopping</S.Title>
+      </S.LogoWrapper>
+      <S.HamburgerButton src={menuImg} onClick={handleMenuOpen} />
       {isMenuOpened && <Dropdown handleMenuOpen={handleMenuOpen} />}
-    </Head>
+    </S.Head>
   );
 }
